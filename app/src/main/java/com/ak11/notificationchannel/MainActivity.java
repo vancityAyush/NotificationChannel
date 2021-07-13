8ce4ea86775a4c49db35091432c2ed62aa25dc3d
vancityAyush <eliasakeleven@gmail.com>
2021-07-14 05:25:52 +0530

    commit #2

--- app/src/main/java/com/ak11/notificationchannel/MainActivity.java
+++ app/src/main/java/com/ak11/notificationchannel/MainActivity.java
@@ -3,7 +3,9 @@ package com.ak11.notificationchannel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 postNotificationToUserDevice(WATCH_MOVIE_NOTIFICATION_ID,edtWatchMovie.getText().toString());
                 break;
             case R.id.btnMovieSettings:
+                openNotificationSettingsForWatchMovieChannel(notificationHandler.WATCH_MOVIE_NOTIFICATION_CHANNEL_ID);
                 break;
 
 
@@ -56,6 +59,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             notificationHandler.notifyTheUser(WATCH_MOVIE_NOTIFICATION_ID,notificationBuilder);
         }
 
+
+    }
+
+    private void openNotificationSettingsForWatchMovieChannel(String watchMovieChannel){
+        Intent settingsIntent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+        settingsIntent.putExtra(Settings.EXTRA_APP_PACKAGE,getPackageName());
+        settingsIntent.putExtra(Settings.EXTRA_CHANNEL_ID,watchMovieChannel);
+        startActivity(settingsIntent);
 
     }
 }
